ead20b4e6c70e9e93f6fe9452d9d6894e92d79b7
Java Software <pj.midgard@yahoo.com>
2021-08-15 11:11:54 +0100

    Add files via upload

--- Portal8.py
+++ Portal8.py
@@ -3,7 +3,7 @@ cvf=0
 Portal=2
 import os
 import binascii
-namez = input("ul,c or for compress cl,u for extract for compress zst cld fo extract zst cldd? ")
+namez = input("ul or for compress cl for extract for compress zst cld fo extract zst cldd? ")
 #@Author Jurijus pacalovas
 class compression:
     
